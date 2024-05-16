5f7a057dfc6bd1e6f42d1ac32255df746d59607d
ManuelGioino <162721952+ManuelGioino@users.noreply.github.com>
2024-05-16 19:35:05 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -28,8 +28,8 @@ def is_empty(list_to_check):
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
         return (list_to_compare1[2] == list_to_compare2[2])
-    else:
-        return False
+else:
+    return False
 
 
 def list_of_lists(list_of_lists_to_modify):
